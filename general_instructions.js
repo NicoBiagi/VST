6c971c8e920ef8153518457fbbd770a9f130d329
Nico Biagi <54856105+NicoBiagi@users.noreply.github.com>
2023-05-16 15:18:29 +0100

    adding webgazer

--- general_instructions.js
+++ general_instructions.js
@@ -2,8 +2,8 @@ var general_instructions = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: "<h1>General instructions: Individual differences in affective responses</h1><br>"+
     "<div style='text-align: left; width:80%; margin:auto;'><p>The goal of the task is first to identify, from four different strings of letters,the letter string that is different from the rest.</p>"+
-    "<p>Press the 'm' key if the different letter string is an English word.</p>"+
-    "<p>Press the 'z' key if the different letter string is a non word.</p>"+
+    "<p>Press the <strong>'m' key </strong> if the different letter string is <strong>an English word.</strong></p>"+
+    "<p>Press the <strong>'z' key </strong> if the different letter string is <strong>a non word</strong>.</p>"+
     "<p>Please press any key to continue </p>"
 };
 
