bbf1c0cfc22213ae38a3472613366d9043b3441d
Nico Biagi <54856105+NicoBiagi@users.noreply.github.com>
2023-06-15 15:41:31 +0100

    adding practice trials and more instructions.

---
+++ practice_end.js
@@ -0,0 +1,6 @@
+var practice_end = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus:  "<div style='text-align: left; width:100%; margin:auto;'><p>You have now reached the end of the practice trial.</p>"+
+    "<p>Please press any key to start the main task.</p>"
+};
+
---
+++ practice_instructions.js
@@ -0,0 +1,6 @@
+var practice_instructions = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus: "<div style='text-align: left; width:80%; margin:auto;'><p>We will provide you with several practice trials where you will receive feedback on your performance after selecting either 'z' or 'm' on the screen. However, this feedback will not be given during the main part of the task.</p>"+
+    "<p>Please press any key to continue </p>"
+};
+
