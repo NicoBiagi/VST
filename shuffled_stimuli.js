63a0e3857851ded437fa6c63c3246027475de0db
nico <nico.biagi@gmail.com>
2023-07-13 16:39:31 +0200

    CR.shift() instead of CR.pop()

--- shuffled_stimuli.js
+++ shuffled_stimuli.js
@@ -1,4 +1,77 @@
-var shuffled_stimuli = [
+var shuffled_stimuli = 
+[
+  {
+    "index": 0,
+    "Condition Index": "a",
+    "Condition Type": "Facilitation Condition Trials",
+    "Stimuli Combination": "uncertainty word target + non-word distractor",
+    "target": "unsure",
+    "distractor1": "wrflz",
+    "distractor2": "wrflz",
+    "distractor3": "wrflz",
+    "correct_answer": "m",
+    "stimuli": "unsure,wrflz,wrflz,wrflz"
+  },
+  {
+    "index": 1,
+    "Condition Index": "b",
+    "Condition Type": "Facilitation Condition Trials",
+    "Stimuli Combination": "categorized neutral word target + non-word distractor",
+    "target": "utensil",
+    "distractor1": "kfftyn",
+    "distractor2": "kfftyn",
+    "distractor3": "kfftyn",
+    "correct_answer": "m",
+    "stimuli": "utensil,kfftyn,kfftyn,kfftyn"
+  },
+  {
+    "index": 2,
+    "Condition Index": "c",
+    "Condition Type": "Facilitation Condition Trials",
+    "Stimuli Combination": "non-word target + non-word distractor",
+    "target": "szknnjbt",
+    "distractor1": "gwmwql",
+    "distractor2": "gwmwql",
+    "distractor3": "gwmwql",
+    "correct_answer": "z",
+    "stimuli": "szknnjbt,gwmwql,gwmwql,gwmwql"
+  },
+  {
+    "index": 3,
+    "Condition Index": "d",
+    "Condition Type": "Disengagement Condition Trials",
+    "Stimuli Combination": "non-word target + uncertainty word distractor",
+    "target": "qddkgm",
+    "distractor1": "random",
+    "distractor2": "random",
+    "distractor3": "random",
+    "correct_answer": "z",
+    "stimuli": "qddkgm,random,random,random"
+  },
+  {
+    "index": 4,
+    "Condition Index": "e",
+    "Condition Type": "Disengagement Condition Trials",
+    "Stimuli Combination": "non-word target + categorised neutral word distractor",
+    "target": "djmrtb",
+    "distractor1": "picture",
+    "distractor2": "picture",
+    "distractor3": "picture",
+    "correct_answer": "z",
+    "stimuli": "djmrtb,picture,picture,picture"
+  },
+  {
+    "index": 5,
+    "Condition Index": "f",
+    "Condition Type": "Disengagement Condition Trials",
+    "Stimuli Combination": "uncategorized neutral word target + uncategorized neutral distractor",
+    "target": "unused",
+    "distractor1": "market",
+    "distractor2": "market",
+    "distractor3": "market",
+    "correct_answer": "m",
+    "stimuli": "unused,market,market,market"
+  },
   {
     "index": 17,
     "Condition Index": "d",
